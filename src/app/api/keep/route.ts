d94a3df1c3001f960b6123957210cae972f497a0
LachlanSleight <lachlansleight@gmail.com>
2026-06-06 16:10:47 +0100

    feat: Make google keep endpoint actually useful

--- src/app/api/keep/route.ts
+++ src/app/api/keep/route.ts
@@ -6,24 +6,32 @@ export const revalidate = 0;
 export const GET = async () => {
     const keep = new Keep();
     await keep.authenticate("lachlansleight@gmail.com", process.env.GOOGLE_MASTER_TOKEN as string);
+    const lists: {
+        name: string;
+        todo: string[];
+    }[] = [];
     const notes = keep.all().map(node => {
         if (node instanceof List) {
-            return {
-                type: "list" as const,
-                id: node.id,
-                title: node.title,
-                items: node.items.map(item => ({
-                    text: item.text,
-                    checked: item.checked,
-                })),
-            };
+            lists.push({
+                name: node.title,
+                todo: node.items.filter(i => !i.checked).map(i => i.text),
+            });
+            // return {
+            //     type: "list" as const,
+            //     id: node.id,
+            //     title: node.title,
+            //     items: node.items.map(item => ({
+            //         text: item.text,
+            //         checked: item.checked,
+            //     })),
+            // };
         }
-        return {
-            type: "note" as const,
-            id: node.id,
-            title: node.title,
-            text: node.text,
-        };
+        // return {
+        //     type: "note" as const,
+        //     id: node.id,
+        //     title: node.title,
+        //     text: node.text,
+        // };
     });
-    return NextResponse.json({ notes });
+    return NextResponse.json({ lists: lists.filter(l => l.todo.length > 0) });
 };
